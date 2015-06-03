7ccb16fc7e1a5f02125fcb57422951f969f42f2f
Ashley Barton <barton.ash@gmail.com>
2015-06-03 01:47:48 -0700

    Main landing activity working, starting work on instruction activity

--- app/src/main/java/com/example/android/makeful/MainActivityFragment.java
+++ app/src/main/java/com/example/android/makeful/MainActivityFragment.java
@@ -1,7 +1,8 @@
 package com.example.android.makeful;
 
-import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +22,8 @@ public class MainActivityFragment extends Fragment {
     private ArrayList<String> projects;
     private ArrayAdapter<String> userInputAdapter;
     private Button buttonAdd;
+    private Button makeCupcakes;
+
     private EditText et;
     private ListView lv;
 
@@ -32,19 +35,15 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-        projects = new ArrayList<String>();
 
-        //button that, on click, will add user input to the listview
-        //buttonAdd = (Button) rootView.findViewById(R.id.add_button_user_input);
-        //*****buttonAdd.setOnClickListener();
-
-        //edit text that will pass user input to the list
-        et = (EditText) rootView.findViewById(R.id.edit_text_project_name);
-
-        //creating array adapter to communicate with the listview
-        userInputAdapter = new ArrayAdapter<String>(getActivity(), R.layout.listitem_user_input, R.id.list_item_user_input_textview, projects);
-        lv = (ListView) rootView.findViewById(R.id.list_item);
-        lv.setAdapter(userInputAdapter);
+        makeCupcakes = (Button)rootView.findViewById(R.id.makeCupcakes);
+        makeCupcakes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), InstructionActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return rootView;
     }
