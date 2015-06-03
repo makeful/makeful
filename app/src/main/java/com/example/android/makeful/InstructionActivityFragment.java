81c9da2401a497b6bd002d3699c8b6ea53405856
Ashley Barton <barton.ash@gmail.com>
2015-06-03 01:59:25 -0700

    Instructions are displayed

--- app/src/main/java/com/example/android/makeful/InstructionActivityFragment.java
+++ app/src/main/java/com/example/android/makeful/InstructionActivityFragment.java
@@ -1,10 +1,15 @@
 package com.example.android.makeful;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -12,12 +17,32 @@ import android.view.ViewGroup;
  */
 public class InstructionActivityFragment extends Fragment {
 
+    ArrayAdapter<String> textAdapter;
+    Tree sampleDataStore;
+    Node instructionTree;
+    List<String> instructionText;
+
     public InstructionActivityFragment() {
+        sampleDataStore = new Tree();
+        instructionTree = sampleDataStore.getProject("Cupcakes");
+        List<Instruction> instructions = TreeHelpers.flattenTree(instructionTree);
+        instructionText = new ArrayList<String>();
+        for(Instruction instr : instructions) {
+            instructionText.add(instr.text);
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_instruction, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_instruction, container, false);
+
+        textAdapter = new ArrayAdapter<String>(getActivity(), R.layout.listitem_user_input, R.id.list_item_user_input_textview, instructionText);
+
+
+        ListView instructionList = (ListView)rootView.findViewById(R.id.instructionList);
+        instructionList.setAdapter(textAdapter);
+
+        return rootView;
     }
 }
---
+++ app/src/main/java/com/example/android/makeful/TreeHelpers.java
@@ -0,0 +1,60 @@
+package com.example.android.makeful;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
+/**
+ * Created by ashley on 6/3/15.
+ */
+public class TreeHelpers {
+    public static List<Instruction> flattenTree(Node node){
+        List<Instruction> nodeList = new LinkedList<Instruction>();
+        Queue<Node> queue = new LinkedList<Node>();
+        Set<Node> set = new HashSet<Node>();
+        flattenRec(queue, node);
+        set.add(null);
+
+        int count = 0;
+        Node current = null;
+
+        while(!queue.isEmpty() && count < queue.size()){
+            count+=1;
+            current = queue.remove();
+            //for every node in my queue
+            //check to see if I can add that instruction to my list
+            //(so check if dependencies are met
+            if(isSatisfied(current, set)){
+                if(current instanceof Instruction)
+                    nodeList.add((Instruction)current);
+                set.add(current);
+                count = 0;
+            } else {
+                queue.add(current);
+            }
+        }
+
+        return nodeList;
+    }
+
+
+    //set = met dependencies
+    //queue = list of things I need to do
+    //linked = final order
+    private static boolean isSatisfied(Node n, Set<Node> s){
+        if(n == null || n instanceof Ingredient) return true;
+        return s.contains(n.left) && s.contains(n.right);
+    }
+
+    private static void flattenRec(Queue<Node> q, Node n){
+        if(n == null)
+            return;
+
+        q.add(n);
+        flattenRec(q, n.left);
+        flattenRec(q, n.right);
+
+    }
+}
